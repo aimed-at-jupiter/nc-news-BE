f114a0d530a8226e08af6f83af6273be3bb4e079
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-06-02 21:20:38 +0100

    added timestamp util fn for inserting data

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+### Connecting to the Databases
+
+This project uses environment variables to securely manage database configuration. To run the application locally:
+
+1. Create two `.env` files in the root of the project:
+
+   - `.env.development` for the development database
+   - `.env.test` for the test database
+
+2. Add the `PGDATABASE` variable to each file:
+
+```env
+.env.development
+PGDATABASE=nc_news
+
+.env.test
+PGDATABASE=nc_news_test
+```
+
+`.env.*` files are git-ignored
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,129 @@
-const db = require("../connection")
+const db = require("../connection");
+const format = require("pg-format");
+const { convertTimestampToDate } = require("./utils");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(`DROP TABLE IF EXISTS comments`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics`);
+    })
+    .then(() => {
+      return db.query(
+        `CREATE TABLE topics(slug VARCHAR(50) PRIMARY KEY NOT NULL,
+         description VARCHAR(255) NOT NULL,
+         img_url VARCHAR(1000))`
+      );
+    })
+    .then(() => {
+      return db.query(
+        `CREATE TABLE users(username VARCHAR(30) PRIMARY KEY NOT NULL, 
+         name VARCHAR(50) NOT NULL,
+         avatar_url VARCHAR(1000))`
+      );
+    })
+    .then(() => {
+      return db.query(
+        `CREATE TABLE articles(article_id SERIAL PRIMARY KEY, 
+         title VARCHAR(150) NOT NULL, 
+         topic VARCHAR(50) REFERENCES topics(slug) ON DELETE RESTRICT ,
+         author VARCHAR(30) NULL REFERENCES users(username) ON DELETE SET NULL,
+         body TEXT,
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+         votes INT DEFAULT 0,
+         article_img_url VARCHAR(1000))`
+      );
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments(comment_id SERIAL PRIMARY KEY,
+        article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
+        body TEXT,
+        votes INT DEFAULT 0,
+        author VARCHAR(30) NULL REFERENCES users(username) ON DELETE SET NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`);
+    })
+    .then(() => {
+      const formattedTopicValues = topicData.map(
+        ({ slug, description, img_url }) => {
+          return [slug, description, img_url];
+        }
+      );
+      const topicSqlString = format(
+        `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *`,
+        formattedTopicValues
+      );
+      return db.query(topicSqlString);
+    })
+    .then(() => {
+      const formattedUserValues = userData.map(
+        ({ username, name, avatar_url }) => {
+          return [username, name, avatar_url];
+        }
+      );
+      const userSqlString = format(
+        `INSERT INTO users (username, name, avatar_url) VALUES %L RETURNING *`,
+        formattedUserValues
+      );
+      return db.query(userSqlString);
+    })
+    .then(() => {
+      const articleDataUpdatedDates = articleData.map((article) => {
+        return convertTimestampToDate(article);
+      });
+      const formattedArticleValues = articleDataUpdatedDates.map(
+        ({
+          title,
+          topic,
+          author,
+          body,
+          created_at,
+          votes,
+          article_img_url,
+        }) => {
+          return [
+            title,
+            topic,
+            author,
+            body,
+            created_at,
+            votes,
+            article_img_url,
+          ];
+        }
+      );
+      const articleSqlString = format(
+        `INSERT INTO articles (title,
+            topic,
+            author,
+            body,
+            created_at,
+            votes,
+            article_img_url) VALUES %L RETURNING *`,
+        formattedArticleValues
+      );
+      return db.query(articleSqlString);
+    })
+    .then(() => {
+      const CommentDataUpdatedDates = commentData.map((comment) => {
+        return convertTimestampToDate(comment);
+      });
+      const formattedCommentData = CommentDataUpdatedDates.map(
+        ({ article_id, body, votes, author, created_at }) => {
+          return [article_id, body, votes, author, created_at];
+        }
+      );
+      const commentSqlString = format(
+        `INSERT INTO comments (article_id, body, votes, author, created_at) VALUES %L RETURNING *`,
+        formattedCommentData
+      );
+      return db.query(commentSqlString);
+    });
 };
+
 module.exports = seed;
