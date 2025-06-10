75457a86ff01dbfbfcc51f9c5decf811ba755ef5
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-06-10 19:05:05 +0100

    amending connection

--- db/connection.js
+++ db/connection.js
@@ -6,15 +6,19 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
-}
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
+
+// if (!process.env.PGDATABASE) {
+//   throw new Error("No PGDATABASE configured");
+// } else {
+//   console.log(`Connected to ${process.env.PGDATABASE}`);
+// }
+
+// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+//   throw new Error("PGDATABASE or DATABASE_URL not set");
+// }
 
 const config = {};
 
