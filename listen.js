911fdb34bcf73aaa234eeea1d62699a3700fb37a
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-06-10 18:46:31 +0100

    adding html for API, updating connection, listen and scripts

--- listen.js
+++ listen.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
 const port = 9999;
+const { PORT = 9090 } = process.env;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
