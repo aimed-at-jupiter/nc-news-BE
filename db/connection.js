5cca31efd17034090a6fcee3f23b95307179907b
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-06-23 10:33:31 +0100

    updating connection file

--- db/connection.js
+++ db/connection.js
@@ -4,21 +4,9 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
-
-// if (!process.env.PGDATABASE) {
-//   throw new Error("No PGDATABASE configured");
-// } else {
-//   console.log(`Connected to ${process.env.PGDATABASE}`);
-// }
-
-// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-//   throw new Error("PGDATABASE or DATABASE_URL not set");
-// }
 
 const config = {};
 
@@ -27,5 +15,3 @@ if (ENV === "production") {
   config.max = 2;
 }
 module.exports = new Pool(config);
-
-module.exports = db;
